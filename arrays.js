aa4e9ea40e97ea97633deb64d1b31d245d10a4c7
Louise Norris <louisenorris73@yahoo.co.uk>
2019-03-24 19:45:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray () {
-  chocolateBars.unshift(array, element);
+function addElementToBeginningOfArray (array, element) {
+  chocolateBars.unshift(element, ...array);
 }
