568f37797a1ebfcdb446296375e225a08de788a2
Louise Norris <louisenorris73@yahoo.co.uk>
2019-03-24 20:17:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,8 @@ function destructivelyRemoveElementFromBeginningOfArray (array, element) {
   array.shift(element)
   return array
 }
+
+function removeElementFromBeginningOfArray (array, element) {
+  array.slice(element) 
+  return array 
+}
