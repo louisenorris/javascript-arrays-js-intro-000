4f91aceeb434433a15b780c61cc7553106a8402a
Louise Norris <louisenorris73@yahoo.co.uk>
2019-03-24 19:57:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,7 @@ function destructivelyAddElementToEndOfArray (array, element) {
   array.push(element)
   return array
 }
+
+function accessElementInArray (array, index) {
+  console.log(chocolateBars[0])
+}
